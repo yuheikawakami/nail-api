bfc40d8396c82870800840f9f07eea72f0b2702b
kawakami yuhei <yuhei@my-MBP.local>
2016-03-19 21:58:29 +0900

    Update migration

---
+++ nail-api/database/migrations/2016_03_19_191544_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+          $table->increments('id');
+          $table->integer('user_id')->unsigned();
+          $table->string('nail_menu');
+          $table->datetime('start');
+          $table->datetime('end');
+          $table->timestamps();
+
+          $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('reservations');
+    }
+}
---
+++ nail-api/database/migrations/2016_03_19_195307_points_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class PointsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('hold_point');
+            $table->integer('expire_date');
+            $table->integer('history');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                  ->references('id')
+                  ->on('users')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('points');
+    }
+}
